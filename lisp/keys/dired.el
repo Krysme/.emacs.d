87aa240bb5f7887f3c06d8c1d60834beab594390
Krys <blackshop.s.bossss@gmail.com>
2019-01-22 03:20:11 +0000

    dired key settings

--- lisp/keys/dired.el
+++ lisp/keys/dired.el
@@ -1,3 +1,9 @@
 
+;; disable space
 (after-load 'dired
   (define-key dired-mode-map (kbd "SPC") nil))
+
+;; key redefinition
+(after-load 'dired
+  (define-key dired-mode-map (kbd "C-l") 'dired-up-directory)
+  (define-key dired-mode-map (kbd "TAB") 'dired-find-file))
