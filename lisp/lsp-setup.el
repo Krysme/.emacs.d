a6408db7c9fd40372c90cdcd56efa41b9ecaa29c
Krys <blackshop.s.bossss@gmail.com>
2019-02-20 12:56:26 +0800

    yasnippet

--- lisp/lsp-setup.el
+++ lisp/lsp-setup.el
@@ -5,17 +5,19 @@
 (ensure-package 'lsp-mode)
 (ensure-package 'company-lsp)
 (ensure-package 'lsp-ui)
+(ensure-package 'yasnippet)
 
 (after-load 'lsp-mode
   (after-load 'rust-mode
     (add-hook 'rust-mode-hook 'lsp)))
 
 (after-load 'lsp-mode
-  (setq lsp-prefer-flymake nil)
-  (add-hook
-   'lsp-mode-hook
-   (lambda ()
-     (flycheck-mode t))))
+  (setq lsp-prefer-flymake nil))
+
+
+(after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook (lambda () (flycheck-mode t)))
+  (add-hook 'lsp-mode-hook 'yas-minor-mode))
 
 (after-load 'rust-mode
   (add-hook 'rust-mode-hook 'smartparens-mode))
