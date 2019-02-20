8491d6c1f830574d291e9a974eb3fa81a7d0cb69
Krys <blackshop.s.bossss@gmail.com>
2019-02-20 13:03:52 +0800

    removed installation prompt

--- lisp/package-setup.el
+++ lisp/package-setup.el
@@ -10,8 +10,7 @@
 
 (defun ensure-package (package)
   "ensure that PACKAGE is installed"
-  (unless (or (package-installed-p package)
-	       (not (y-or-n-p (message "Package %s is missing, install it ?" package))))
+  (unless (package-installed-p package)
     (unless package-archive-contents
       (package-refresh-contents))
     (package-install package))
