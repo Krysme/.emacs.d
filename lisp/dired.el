8dc83ac9123afa6c28607655290ff9578286213e
Krys <blackshop.s.bossss@gmail.com>
2019-05-19 20:29:52 +0800

    added dired settings

---
+++ lisp/dired.el
@@ -0,0 +1,10 @@
+(require 'dired)
+(require 'dired-x)
+
+
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+(setq dired-dwim-target t)
+
+
+(put 'dired-find-alternate-file 'disabled nil)
