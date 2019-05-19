af205ee74e1309f0b08b5ec8fb26eaeaa4efb42b
Krys <blackshop.s.bossss@gmail.com>
2019-05-19 20:28:52 +0800

    rust mode has show-paren-mode now

--- lisp/rust.el
+++ lisp/rust.el
@@ -9,7 +9,12 @@
     (add-hook 'rust-mode-hook 'lsp)))
 
 (after-load 'rust-mode
-  (add-hook 'rust-mode-hook 'smartparens-mode))
+  (add-hook 'rust-mode-hook 'smartparens-mode)
+  (add-hook 'rust-mode-hook 'show-paren-mode))
 
 (after-load 'rust-mode
   (rust-enable-format-on-save))
+
+(after-load 'rust-mode
+  (after-load 'smartparens
+	(sp-local-pair 'rust-mode "'" nil :actions nil)))
