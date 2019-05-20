e413d6369e718021061f1d3b152d3f2843e58127
Krys <blackshop.s.bossss@gmail.com>
2019-05-20 16:14:34 +0800

    deleted redundent code in init.el

--- init.el
+++ init.el
@@ -1,15 +1,5 @@
 ;; -*- lexical-binding: t -*-
 
-(defalias 'after-load 'with-eval-after-load)
-(setq debug-on-error t)
-
-(defun load-directory (dir)
-  (let ((load (lambda (f)
-		(load-file (concat (file-name-as-directory dir) f)))))
-    (mapc load (directory-files dir nil "\\.el$"))))
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-
 (load-file (expand-file-name "preload.el" user-emacs-directory)) 
 (load-directory (expand-file-name "lisp" user-emacs-directory))
 (load-directory (expand-file-name "keys" user-emacs-directory))
