769e082976abe1fd766659f6606119944a3b8c1f
Krys <blackshop.s.bossss@gmail.com>
2019-05-20 10:25:30 +0800

    use ag instead of rg for helm-ag

--- lisp/helm.el
+++ lisp/helm.el
@@ -12,9 +12,6 @@
     (setq helm-completion-in-region-fuzzy-match t)
     (setq helm-M-x-fuzzy-match t))
 
-(after-load 'helm-ag
-  (custom-set-variables
-   '(helm-ag-base-command "rg --no-heading")))
 
 
 
