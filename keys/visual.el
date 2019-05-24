8b13728d903944d42abdd6e71d4167dcf65fea73
Krys <blackshop.s.bossss@gmail.com>
2019-05-24 17:54:04 +0800

    set fast moves on evil-motion/evil-visual mode

--- keys/visual.el
+++ keys/visual.el
@@ -2,6 +2,12 @@
 (after-load 'evil
   (define-key evil-visual-state-map (kbd "C-j") 'evil-move-5-lines-down)
   (define-key evil-visual-state-map (kbd "C-k") 'evil-move-5-lines-up)
+  (define-key evil-motion-state-map (kbd "C-j") 'evil-move-5-lines-down)
+  (define-key evil-motion-state-map (kbd "C-k") 'evil-move-5-lines-up)
+  (define-key evil-motion-state-map (kbd "C-e") (lambda () (interactive) (evil-scroll-line-down 5)))
+  (define-key evil-visual-state-map (kbd "C-e") (lambda () (interactive) (evil-scroll-line-down 5)))
+  (define-key evil-motion-state-map (kbd "C-y") (lambda () (interactive) (evil-scroll-line-up 5)))
+  (define-key evil-visual-state-map (kbd "C-y") (lambda () (interactive) (evil-scroll-line-up 5)))
   (define-key evil-motion-state-map (kbd "SPC") nil)
   (define-key evil-visual-state-map (kbd "SPC g") 'google-search)
   (define-key evil-motion-state-map (kbd "SPC g") 'google-search)
