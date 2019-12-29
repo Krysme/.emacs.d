e22dcc98aa0613b51ef3def631837a682a0ad72a
Krys <blackshop.s.bossss@gmail.com>
2019-12-29 11:17:48 +0800

    remove m-p and m-n for company-mode

--- keys/company.el
+++ keys/company.el
@@ -6,4 +6,6 @@
   (define-key company-active-map [tab] 'company-complete-selection)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "C-w") nil))
