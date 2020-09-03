c7f0187505afb48a4f3ccf31b3cfb6098789c6a3
Krys <blackshop.s.bossss@gmail.com>
2020-09-03 16:12:19 +0800

    modify company tab function

--- keys/company.el
+++ keys/company.el
@@ -3,7 +3,7 @@
   (define-key company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common))
 
 (after-load 'company
-  (define-key company-active-map [tab] 'company-complete-selection)
+  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "M-p") nil)
