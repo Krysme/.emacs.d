4d0e16ce075451696d3880f24ac5fb96f0b881fd
Krys <blackshop.s.bossss@gmail.com>
2020-09-08 23:45:35 +0800

    add after-load for ob-rust to avoid crash

--- lisp/org.el
+++ lisp/org.el
@@ -2,8 +2,9 @@
 (ensure-package 'org-bullets)
 (require 'org-tempo)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(org-babel-do-load-languages
- 'org-babel-load-languages '((C . t)
-							 (rust . t)))
+(after-load 'ob-rust
+  (org-babel-do-load-languages
+   'org-babel-load-languages '((C . t)
+							   (rust . t))))
 
 (setq org-hide-emphasis-markers t)
