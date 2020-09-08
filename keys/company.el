a2ad8b1c43deea68efcf8462ac8b86f2ecff22f9
Krys <blackshop.s.bossss@gmail.com>
2020-09-09 06:30:00 +0800

    add lisp-select-only-one-or-select-current to TAB

--- keys/company.el
+++ keys/company.el
@@ -1,9 +1,16 @@
 
-(after-load 'company
-  (define-key company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common))
+;; (after-load 'company
+;;   (define-key company-mode-map [remap indent-for-tab-command] #'company-indent-or-complete-common))
+
+
+(defun lisp-select-only-one-or-select-current()
+  (interactive)
+  (unless  (> company-candidates-length 1)
+	(call-interactively 'company-select-next))
+  (call-interactively 'company-complete-selection))
 
 (after-load 'company
-  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+  (define-key company-active-map [tab] 'lisp-select-only-one-or-select-current)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "M-p") nil)
