6eedd0001dd0d199b187cb69da4c988cea6f4b0d
yangdonglin <332407470@qq.com>
2020-11-06 11:35:05 +0800

    speed up loading

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
+
+(setq gc-cons-threshold 100000000000000)
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 8000000)))
+
 (load-file (expand-file-name "preload.el" user-emacs-directory)) 
 (load-directory (expand-file-name "lisp" user-emacs-directory))
 (load-directory (expand-file-name "keys" user-emacs-directory))
