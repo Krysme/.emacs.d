cddaf4a3bad545838f4baef8671e4b3b52a4d6d7
yangdonglin <332407470@qq.com>
2020-12-02 13:35:45 +0800

    move evil-magit bindings to evil-collection

--- keys/magit.el
+++ keys/magit.el
@@ -1,3 +1,3 @@
-(after-load 'evil-magit
+(after-load 'evil-collection
   (define-key evil-normal-state-map (kbd "SPC m g") 'magit)
   (define-key evil-normal-state-map (kbd "SPC m p") 'magit-dispatch-popup))
