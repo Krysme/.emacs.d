4c418fa83ad07afab486e89c232e23a319dbf30b
yangdonglin <332407470@qq.com>
2021-04-12 10:08:14 +0800

    switch back to spacemacs theme

--- lisp/settings.el
+++ lisp/settings.el
@@ -26,8 +26,8 @@
 
 (display-time-mode t)
 
-;; (unless (package-installed-p 'spacemacs-theme)
-;;   (package-install 'spacemacs-theme))
+(unless (package-installed-p 'spacemacs-theme)
+  (package-install 'spacemacs-theme))
 
 
 (setq tab-always-indent 'complete)
@@ -35,8 +35,8 @@
 (add-hook 'after-change-major-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
 (add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
 
-;; (unless (package-installed-p 'spacemacs-theme)
-;;   (package-install 'spacemacs-theme))
+(unless (package-installed-p 'spacemacs-theme)
+  (package-install 'spacemacs-theme))
 
 (unless (package-installed-p 'poet-theme)
   (package-install 'poet-theme))
