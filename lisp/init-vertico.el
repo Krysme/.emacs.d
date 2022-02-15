c58bd9918cd31cb7c8402e740b043044f69608c0
Krys <blackshop.s.bossss@gmail.com>
2022-02-15 15:12:54 +0800

    basically finished vertico

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -1,3 +1,80 @@
+(straight-use-package 'vertico)
+(straight-use-package 'savehist
+		      :init
+		      (savehist-mode))
 
+
+
+
+(vertico-mode)
+(setq vertico-count 10)
+(setq vertico-resize t)
+(setq vertico-cycle t)
+
+(use-package orderless
+  :ensure t
+  :straight t
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless)
+	completion-category-defaults nil
+	completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package consult
+  :straight t
+  :ensure t)
+
+(after-load 'evil
+  (define-key evil-normal-state-map (kbd "SPC /") (lambda () (interactive) (consult-line)))
+  (define-key evil-normal-state-map (kbd "SPC #") (lambda () (interactive) (consult-line (word-at-point))))
+  (define-key evil-normal-state-map (kbd "SPC *") (lambda () (interactive) (consult-line-reverse (word-at-point))))
+  (define-key evil-normal-state-map (kbd "SPC r") 'consult-recent-file-no-action))
+
+(defun consult-line-reverse (&optional initial start)
+  "This is a reversed version of `consult-line'"
+  (interactive (list nil (not (not current-prefix-arg))))
+  (let ((curr-line (line-number-at-pos (point) consult-line-numbers-widen))
+	(top (not (eq start consult-line-start-from-top))))
+    (consult--line
+     (or (consult--with-increased-gc
+	  (utils-reverse-cdr (consult--line-candidates top curr-line)))
+	 (user-error "No lines"))
+     :curr-line (and (not top) curr-line)
+     :prompt (if top "Go to line from top: " "Go to line: ")
+     :initial initial)))
+
+(defun consult-recent-file-no-action ()
+  "Find recent file using `completing-read'."
+  (interactive)
+  (find-file
+   (consult--read
+    (or (mapcar #'abbreviate-file-name recentf-list)
+	(user-error "No recent files, `recentf-mode' is %s"
+		    (if recentf-mode "on" "off")))
+    :prompt "Find recent file: "
+    :sort nil
+    :require-match t
+    :category 'file
+    :state nil
+    :history 'file-name-history)))
+
+(use-package marginalia
+  :straight t
+  :ensure t
+  :config
+  (marginalia-mode))
+
+
+(defun vertico-up-directory ()
+  "go up one directory"
+  (interactive)
+  (let ((directory (minibuffer-contents-no-properties)))
+    (delete-minibuffer-contents)
+    (insert (string-trim-right (if (string= directory "~/") (expand-file-name "~/") directory) "[^/]+/?"))))
+
+
+(define-key minibuffer-local-map (kbd "C-l") #'vertico-up-directory)
 
 (provide 'init-vertico)
