4668c6bd0690c79514345d5ea8aaa7c52a465798
Krys <332407470@qq.com>
2022-03-23 01:52:40 +0800

    format

--- lisp/init-paredit.el
+++ lisp/init-paredit.el
@@ -6,23 +6,29 @@
 
 (add-hook 'paredit-mode-hook 'rainbow-delimiters-mode)
 
-(defun lisp-comment-sexp ()
-  "comment out the sexp at point."
-  (interactive)
-  (save-excursion
-    (mark-sexp)
-    (paredit-comment-dwim)))
+(defun lisp-comment-sexp () 
+  "comment out the sexp at point." 
+  (interactive) 
+  (save-excursion (mark-sexp) 
+		  (paredit-comment-dwim)))
 
 
+(defun paredit-kill-and-insert () 
+  (interactive) 
+  (call-interactively 'paredit-kill) 
+  (call-interactively 'evil-insert))
 
-(defun paredit-custom-leader-keys()
-  (define-key evil-normal-state-map (kbd "SPC sw") 'transpose-sexps)
-  (define-key evil-normal-state-map (kbd "SPC sl") 'paredit-forward-slurp-sexp)
-  (define-key evil-normal-state-map (kbd "SPC al") 'paredit-forward-barf-sexp)
-  (define-key evil-normal-state-map (kbd "SPC sf") 'paredit-forward-barf-sexp)
-  (define-key evil-normal-state-map (kbd "SPC sr") 'paredit-raise-sexp)
-  (define-key evil-normal-state-map (kbd "SPC s;") 'lisp-comment-sexp)
-  (define-key evil-normal-state-map (kbd "SPC sh") 'paredit-backward-barf-sexp)
-  (define-key evil-normal-state-map (kbd "SPC ss") 'paredit-split-sexp))
+
+(defun paredit-custom-leader-keys() 
+  (define-key evil-normal-state-map (kbd "SPC sw") 'transpose-sexps) 
+  (define-key evil-normal-state-map (kbd "SPC sl") 'paredit-forward-slurp-sexp) 
+  (define-key evil-normal-state-map (kbd "SPC al") 'paredit-forward-barf-sexp) 
+  (define-key evil-normal-state-map (kbd "SPC sf") 'paredit-forward-barf-sexp) 
+  (define-key evil-normal-state-map (kbd "SPC sr") 'paredit-raise-sexp) 
+  (define-key evil-normal-state-map (kbd "SPC s;") 'lisp-comment-sexp) 
+  (define-key evil-normal-state-map (kbd "SPC sh") 'paredit-backward-barf-sexp) 
+  (define-key evil-normal-state-map (kbd "SPC ss") 'paredit-split-sexp) 
+  (define-key evil-normal-state-map (kbd "D") 'paredit-kill) 
+  (define-key evil-normal-state-map (kbd "C") 'paredit-kill-and-insert))
 
 (provide 'init-paredit)
