5633016738a0cf2069496a46c9bca9994f9cdad1
Krys <332407470@qq.com>
2022-03-23 01:53:50 +0800

    elisp format

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -4,11 +4,14 @@
 
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
 
+(straight-use-package 'elisp-format)
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-f") 'elisp-format-buffer)
 
 (add-hook 'paredit-mode-hook 'paredit-custom-leader-keys)
 
-(after-load 'smartparens
-  (sp-local-pair 'elisp-mode "'" nil :actions nil))
+(after-load 'smartparens (sp-local-pair 'elisp-mode "'" nil 
+					:actions nil))
 
 
 (provide 'init-elisp)
