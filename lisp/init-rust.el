875d7671a99acb08b2fa37098559519d3ff4f990
yangdonglin <332407470@qq.com>
2022-06-13 14:30:28 +0800

    format

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -10,6 +10,7 @@
 (setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
 (setq lsp-rust-features [])
 (setq lsp-rust-analyzer-proc-macro-enable t)
+(setq lsp-rust-analyzer-experimental-proc-attr-macros t)
 
 (setq lsp-response-timeout 100)
 
@@ -17,41 +18,48 @@
 
 (add-hook 'rust-mode-hook 'smartparens-mode)
 (add-hook 'rust-mode-hook 'show-paren-mode)
-(add-hook 'rust-mode-hook (lambda () (setq tab-width 4)))
+(add-hook 'rust-mode-hook 
+	  (lambda () 
+	    (setq tab-width 4)))
 
 
 
 (add-hook 'toml-mode-hook 'smartparens-mode)
 
-(add-hook 'toml-mode-hook (lambda ()
-			    (define-key evil-visual-state-map (kbd "(") 'sp-wrap-round)
-			    (define-key evil-visual-state-map (kbd ")") 'sp-wrap-square)
-			    (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly)
-			    (define-key evil-visual-state-map (kbd "\"") (lambda () (interactive) (sp-wrap-with-pair "\"")))))
-
-
-(after-load 'smartparens
-  (sp-local-pair 'rust-mode "'" nil :actions nil)
-  (sp-local-pair 'rust-mode "<" ">")
-  (sp-local-pair 'toml-mode "\"" "\"")
-  (sp-local-pair 'rust-mode "\"" "\""))
-  
-
-(add-hook 'rust-mode-hook
+(add-hook 'toml-mode-hook 
 	  (lambda () 
-	    (define-key rust-mode-map (kbd "C-c C-b") 'cargo-process-build)
-	    (define-key rust-mode-map (kbd "C-c C-r") 'cargo-process-run)
+	    (define-key evil-visual-state-map (kbd "(") 'sp-wrap-round) 
+	    (define-key evil-visual-state-map (kbd ")") 'sp-wrap-square) 
+	    (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly) 
+	    (define-key evil-visual-state-map (kbd "\"") 
+	      (lambda () 
+		(interactive) 
+		(sp-wrap-with-pair "\"")))))
 
-	    (define-key evil-visual-state-map (kbd "(") 'sp-wrap-round)
-	    (define-key evil-visual-state-map (kbd ")") 'sp-wrap-square)
-	    (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly)
-	    (define-key evil-visual-state-map (kbd "<") (lambda () (interactive) (sp-wrap-with-pair "<")))))
+
+(after-load 'smartparens (sp-local-pair 'rust-mode "'" nil 
+					:actions nil) 
+	    (sp-local-pair 'rust-mode "<" ">") 
+	    (sp-local-pair 'toml-mode "\"" "\"") 
+	    (sp-local-pair 'rust-mode "\"" "\""))
+
+
+(add-hook 'rust-mode-hook 
+	  (lambda () 
+	    (define-key rust-mode-map (kbd "C-c C-b") 'cargo-process-build) 
+	    (define-key rust-mode-map (kbd "C-c C-r") 'cargo-process-run)
+	    (define-key evil-visual-state-map (kbd "(") 'sp-wrap-round) 
+	    (define-key evil-visual-state-map (kbd ")") 'sp-wrap-square) 
+	    (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly) 
+	    (define-key evil-visual-state-map (kbd "<") 
+	      (lambda () 
+		(interactive) 
+		(sp-wrap-with-pair "<")))))
 
 
 (setq cargo-process--command-run "run --release")
 (setq cargo-process--command-clippy "clippy")
 
-(after-load 'rust-mode
-  (rust-enable-format-on-save))
+(after-load 'rust-mode (rust-enable-format-on-save))
 
 (provide 'init-rust)
