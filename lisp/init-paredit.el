078c9142bdb6280c4953ccb6e0b9d8351def4b63
yangdonglin <332407470@qq.com>
2022-07-01 16:22:15 +0800

    remove C-w for paredit

--- lisp/init-paredit.el
+++ lisp/init-paredit.el
@@ -29,8 +29,7 @@
   (define-key evil-normal-state-map (kbd "SPC sh") 'paredit-backward-barf-sexp) 
   (define-key evil-normal-state-map (kbd "SPC ss") 'paredit-split-sexp) 
   (define-key evil-normal-state-map (kbd "D") 'paredit-kill) 
-  (define-key evil-normal-state-map (kbd "C") 'paredit-kill-and-insert)
-  (define-key evil-insert-state-map (kbd "C-w") 'paredit-backward-kill-word))
+  (define-key evil-normal-state-map (kbd "C") 'paredit-kill-and-insert))
 
 
 (provide 'init-paredit)
