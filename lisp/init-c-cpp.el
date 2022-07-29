c1dfcb5ab463dbcc96cb8d0b0413898f75c48d81
Krys <blackshop.s.bossss@gmail.com>
2022-07-29 10:50:07 +0800

    C++ indent

--- lisp/init-c-cpp.el
+++ lisp/init-c-cpp.el
@@ -8,8 +8,7 @@
 
 (after-load 'smartparens
   (dolist (m init-c-cpp-modes)
-    (sp-local-pair m "'" "'")
-    (sp-local-pair m "<" ">")))
+    (sp-local-pair m "'" "'")))
 
 (add-hook 'c-mode-hook '配平括号)
 (add-hook 'c-mode-hook 'smartparens-mode)
@@ -20,12 +19,18 @@
   "如题"
   (define-key evil-visual-state-map (kbd "(") 'sp-wrap-round)
   (define-key evil-visual-state-map (kbd ")") 'sp-wrap-square)
-  (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly)
-  (define-key evil-visual-state-map (kbd "<") (lambda () (interactive) (sp-wrap-with-pair "<"))))
+  (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly))
 
 (after-load 'yasnippet-snippets
   (add-hook 'c++-mode-hook 'yas-minor-mode-on)
   (add-hook 'c-mode-hook 'yas-minor-mode-on))
 
 
+(define-key c-mode-map (kbd "C-c C-b") 'compile-qt)
+(define-key c++-mode-map (kbd "C-c C-b") 'compile-qt)
+
+(defun compile-qt () (interactive)
+       (compile (concat "cd \"" (lsp-workspace-root ) "\"" " && " "qmake-qt5" " && " "make -j13")))
+
 (provide 'init-c-cpp)
+(setq c-default-style "linux" c-basic-offset 4)
