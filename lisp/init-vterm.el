1f57bf04cb3b399db0c8fa1e2fc923fba8b5538a
yangdonglin <332407470@qq.com>
2022-08-23 14:05:54 +0800

    vterm c-g self insert

--- lisp/init-vterm.el
+++ lisp/init-vterm.el
@@ -4,7 +4,7 @@
 
 (add-hook 'vterm-mode-hook (lambda () (evil-emacs-state)))
 
-;;(define-key vterm-mode-map (kbd "C-g") 'vterm--self-insert)
+(define-key vterm-mode-map (kbd "C-g") 'vterm--self-insert)
 
 (global-set-key (kbd (if (display-graphic-p)  "C-4" "C-\\")) 'vterm-other-window)
 
