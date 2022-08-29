dc713c905b6c255fd4c7d03062a85212c9b96839
yangdonglin <332407470@qq.com>
2022-08-29 15:38:31 +0800

    move function find-file-in-project

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -16,4 +16,25 @@
 	 (rev-rest (reverse rest)))
     (cons first rev-rest)))
 
+
+
+(defun find-file-in-project ()
+  (interactive)
+  (-->
+     (locate-dominating-file default-directory ".git")
+       (or it (user-error "Not in a project"))
+       (expand-file-name it)
+       (shell-command-to-string  (concat  "fd . --type f " "\"" it "\""))
+       (split-string it "[\r\n]+")
+       (seq-filter (lambda (x) (-> x string-blank-p not)) it)
+       (consult--read
+	it
+	:prompt "Find File in Project: "
+	:sort nil
+	:require-match t
+	:category 'file
+	:state nil
+	:history 'file-name-history)
+       (find-file it)))
+
 (provide 'init-utils)
