40ded79ed3d9005077a6b93472581965a675c6f2
yangdonglin <332407470@qq.com>
2022-08-29 11:46:09 +0800

    add mark-outer-sexp for lisp

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -7,8 +7,20 @@
 
 (add-hook 'paredit-mode-hook 'paredit-custom-leader-keys)
 
+
 (after-load 'smartparens (sp-local-pair 'elisp-mode "'" nil 
 					:actions nil))
 
+(defun mark-outer-sexp ()
+  (interactive)
+  (call-interactively 'backward-up-list)
+  (call-interactively 'evil-exit-visual-state)
+  (call-interactively 'evil-jump-item)
+  (call-interactively 'evil-visual-char)
+  (call-interactively 'evil-jump-item))
+
+(add-hook 'emacs-lisp-mode-hook
+	  (evil-define-key 'insert emacs-lisp-mode-map (kbd "C-w") 'backward-kill-sexp)
+	  (evil-define-key 'visual emacs-lisp-mode-map (kbd "v") 'mark-outer-sexp))
 
 (provide 'init-elisp)
