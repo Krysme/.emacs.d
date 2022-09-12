f76c3c654babb0e79e8597a6f7f59b08a6175cdc
Krys <332407470@qq.com>
2022-09-13 00:35:13 +0800

    simplify lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -9,8 +9,9 @@
 (straight-use-package 'lsp-ui)
 (straight-use-package 'smartparens)
 (straight-use-package 'flycheck)
-
 (require 'dash)
+
+
 (after-load 'lsp-mode
   (add-hook 'lsp-mode-hook (lambda () (flycheck-mode t))))
 
@@ -18,33 +19,37 @@
   (setq lsp-enable-snippet t)
   (add-hook 'lsp-mode-hook 'yas-minor-mode))
 
-(setq krys-ui-doc-show t)
 (setq lsp-ui-doc-show-with-cursor t)
 (setq lsp-ui-doc-position 'at-point)
 (setq lsp-ui-sideline-show-code-actions t)
 
-;; keys
-(add-hook
- 'lsp-mode-hook
- (lambda () 
-   (add-hook 'before-save-hook 'lsp-format-buffer)
+(setq krys-ui-doc-show t)
+(defun toggle-lsp-ui-doc ()
+  (interactive)
+  (if krys-ui-doc-show
+      (progn
+	(lsp-ui-doc-enable nil)
+	(lsp-ui-doc-hide)
+	(setq krys-ui-doc-show nil))
+    (progn
+      (lsp-ui-doc-enable t)
+      (lsp-ui-doc-show)
+      (setq krys-ui-doc-show t))))
+
+
+(defun init-lsp-set-keys () 
+   (add-hook 'before-save-hook 'lsp-format-buffer nil t)
    (define-key lsp-mode-map (kbd "C-c C-f") 'lsp-format-buffer)
    (define-key lsp-mode-map (kbd "C-C C-c") 'lsp-ui-sideline-apply-code-actions)
    (define-key lsp-mode-map (kbd "C-C r") 'lsp-find-references)
    (after-load 'evil 
-     (define-key evil-normal-state-map (kbd "SPC o") 'lsp-rename)
-     (define-key evil-visual-state-map (kbd "v") 'lsp-extend-selection)
-     (define-key evil-normal-state-map (kbd "SPC i") (lambda () (interactive)
-						       (if krys-ui-doc-show
-							   (progn
-							     (lsp-ui-doc-enable nil)
-							     (lsp-ui-doc-hide)
-							     (setq krys-ui-doc-show nil))
-							 (progn
-							   (lsp-ui-doc-enable t)
-							   (lsp-ui-doc-show)
-							   (setq krys-ui-doc-show t)))
-						       )))))
+     (evil-define-key 'normal lsp-mode-map (kbd "SPC o") 'lsp-rename)
+     (evil-define-key 'visual lsp-mode-map (kbd "v") 'lsp-extend-selection)
+     (evil-define-key 'normal lsp-mode-map (kbd "SPC i") 'toggle-lsp-ui-doc)))
+
+;; keys
+(add-hook 'lsp-mode-hook 'init-lsp-set-keys)
+
 
 
 
