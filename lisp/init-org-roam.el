22bc9812dd03869cebf8a1b08cfbb9f119ac88d6
yangdonglin <donglin.yang@bkunyun.com>
2023-01-05 17:09:30 +0800

    org-roam

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -1,5 +1,10 @@
 (setq org-roam-directory (file-truename "~/code/roam"))
 (straight-use-package 'org)
 (straight-use-package 'org-roam)
+(straight-use-package 'org-bullets) 
+(setq org-agenda-files '("~/agenda.org"))
+
+
+(add-hook 'org-mode-hook 'org-bullets-mode)
 (org-roam-db-autosync-mode)
 (provide 'init-org-roam)
