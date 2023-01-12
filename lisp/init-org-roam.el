bb8dc443f10be8c00817b9f6b43833fd33a01585
yangdonglin <donglin.yang@bkunyun.com>
2023-01-12 16:56:58 +0800

    org-roam

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -5,6 +5,12 @@
 (setq org-agenda-files '("~/agenda.org"))
 
 
+
+(straight-use-package 'calfw)
+(straight-use-package 'calfw-org)
+(require 'calfw)
+(require 'calfw-org)
+
 (add-hook 'org-mode-hook 'org-bullets-mode)
 (org-roam-db-autosync-mode)
 (provide 'init-org-roam)
