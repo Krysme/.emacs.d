5b4ebbafe64d40ea6b87a9ba5debbb8b576acaaf
yangdonglin <donglin.yang@bkunyun.com>
2023-01-28 13:42:56 +0800

    increase gc-cons-threashold and read-process-output-max

--- lisp/preload.el
+++ lisp/preload.el
@@ -21,6 +21,10 @@
 (straight-use-package 'use-package)
 (use-package el-patch :straight t)
 
+(setq read-process-output-max (* 16 1024 1024))
+
+(setq gc-cons-threshold (* 128 1024 1024))
+
 (setq gc-original-threashold gc-cons-threshold)
 (setq gc-cons-threshold (* 1024 1024 1024))
 
