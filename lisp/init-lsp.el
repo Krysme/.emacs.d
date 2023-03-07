4e958a2816a15c6d26cacbd92fafc721e9869ec6
yangdonglin <donglin.yang@bkunyun.com>
2023-03-07 18:03:41 +0800

    fixed extending selection problem

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -14,7 +14,6 @@
 
 
 
-
 (after-load 'lsp-mode
     (add-hook 'lsp-mode-hook (lambda () (flycheck-mode t))))
 
@@ -44,17 +43,22 @@
 
 (setq xref-prompt-for-identifier nil)
 (defun init-lsp-set-keys () 
-   (add-hook 'before-save-hook 'lsp-format-buffer nil t)
-   (define-key lsp-mode-map (kbd "C-c C-f") 'lsp-format-buffer)
-   (define-key lsp-mode-map (kbd "C-C C-c") 'lsp-ui-sideline-apply-code-actions)
-   (define-key lsp-mode-map (kbd "C-C C-l") 'lsp-ui-imenu)
-   (after-load 'evil 
-     (evil-define-key 'normal lsp-mode-map (kbd "SPC g r") 'xref-find-references)
-     (evil-define-key 'normal lsp-mode-map (kbd "SPC g t") 'lsp-find-type-definition)
-     (evil-define-key 'normal lsp-mode-map (kbd "SPC o") 'lsp-rename)
-     (evil-define-key 'visual lsp-mode-map (kbd "v") 'lsp-extend-selection)
-     (evil-define-key 'normal lsp-mode-map (kbd "K") 'toggle-lsp-ui-doc)))
-
+    (add-hook 'before-save-hook 'lsp-format-buffer nil t)
+    (define-key lsp-mode-map (kbd "C-c C-f") 'lsp-format-buffer)
+    (define-key lsp-mode-map (kbd "C-C C-c") 'lsp-ui-sideline-apply-code-actions)
+    (define-key lsp-mode-map (kbd "C-C C-l") 'lsp-ui-imenu)
+    (after-load 'evil 
+	(evil-define-key 'normal lsp-mode-map (kbd "SPC g r") 'xref-find-references)
+	(evil-define-key 'normal lsp-mode-map (kbd "SPC g t") 'lsp-find-type-definition)
+	(evil-define-key 'normal lsp-mode-map (kbd "SPC o") 'lsp-rename)
+	(evil-define-key 'visual lsp-mode-map (kbd "v") 'lsp-extend-selection)
+	(evil-define-key 'normal lsp-mode-map (kbd "K") 'toggle-lsp-ui-doc)
+	(after-load 'lsp-treemacs
+	    (evil-define-key 'normal lsp-mode-map (kbd "C-c g") 'treemacs)
+	    (evil-define-key 'normal lsp-mode-map (kbd "C-c l") 'lsp-treemacs--build-error-list)
+	    (evil-define-key 'normal lsp-mode-map (kbd "C-c k") 'lsp-treemacs-symbols))))
+(after-load 'evil
+    (add-hook 'evil-visual-state-exit-hook (lambda () (setq lsp--document-selection-range-cache nil))))
 ;; keys
 (add-hook 'lsp-mode-hook 'init-lsp-set-keys)
 
