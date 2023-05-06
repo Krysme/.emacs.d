7f8da3c46c2a3119daa202573d9a0bbee70ea871
Krys <blackshop.s.bossss@gmail.com>
2023-05-06 22:58:54 +0800

    add git gutter

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,4 +2,9 @@
 (straight-use-package 'evil-magit)
 (after-load 'magit (define-key magit-status-mode-map (kbd "SPC") nil))
 
+
+
+(straight-use-package 'git-gutter-fringe)
+(global-git-gutter-mode +1)
+
 (provide 'init-git)
