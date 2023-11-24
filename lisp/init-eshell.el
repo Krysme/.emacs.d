dec4fcd72aa7944170ddf83699ed86de6a3365ce
U-GLOBAL\yangdl <yangdl@BJ-PRIM121.global.primarius-tech.com>
2023-11-24 16:13:15 +0800

    add eshell goodies

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -7,7 +7,11 @@
 
 (add-hook 'eshell-mode-hook
     (lambda ()
+	(company-mode -1)
+	(setq completion-in-region-function #'consult-completion-in-region)
 	(define-key evil-insert-state-map (kbd "C-d") 'eshell-life-is-too-much)
-	(define-key evil-insert-state-map (kbd "C-l") 'eshell-clear-buffer)))
-;; (eshell-previous-matching-input-from-input ARG)
+	(define-key evil-insert-state-map (kbd "C-l") 'eshell-clear-buffer)
+	(define-key evil-insert-state-map (kbd "C-p") 'eshell-previous-matching-input-from-input)
+	(define-key evil-insert-state-map (kbd "C-n") 'eshell-next-matching-input-from-input)))
+
 (provide 'init-eshell)
