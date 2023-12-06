ba4218fbfbb31816d2e2af143bad9805d0336ab1
U-GLOBAL\yangdl <yangdl@BJ-PRIM121.global.primarius-tech.com>
2023-12-06 13:59:59 +0800

    fix indenting

--- lisp/init-c-cpp.el
+++ lisp/init-c-cpp.el
@@ -5,74 +5,72 @@
 (setq init-c-cpp-modes '(c-mode c++-mode cc-mode))
 
 (after-load 'smartparens (dolist (m init-c-cpp-modes)
-(sp-local-pair m "'" "'")))
+			   (sp-local-pair m "'" "'")))
 
 (setq c-default-style "linux" c-basic-offset 4)
 
 (add-hook 'c-mode-hook 'smartparens-mode)
 (add-hook 'c++-mode-hook 'smartparens-mode)
 
-
 (after-load 'yasnippet-snippets (add-hook 'c++-mode-hook 'yas-minor-mode-on)
-(add-hook 'c-mode-hook 'yas-minor-mode-on))
+	    (add-hook 'c-mode-hook 'yas-minor-mode-on))
 
 
 (defun trim-prefix (str ch)
-    "in a string STR, if there is a prefix that maches CH, trim it"
-    (if (and (stringp str) (> (length str) 0) (char-equal (aref str 0) ch))
-	(substring str 1)
-	str))
+  "in a string STR, if there is a prefix that maches CH, trim it"
+  (if (and (stringp str) (> (length str) 0) (char-equal (aref str 0) ch))
+    (substring str 1)
+    str))
 
 
 
 (defun locate-last-dominating-file (directory file-name)
-    "find the topmost directory where the file: FILE-NAME exists starting at DIRECTORY"
-    (let* ((dir directory)
-	      result)
-	(while (not (string= dir (directory-file-name dir)))
-	    (let ((file-path (expand-file-name file-name dir)))
-		(when (file-exists-p file-path)
-		    (setq result file-path))
-		(setq dir (file-name-directory (directory-file-name dir)))))
-	result))
+  "find the topmost directory where the file: FILE-NAME exists starting at DIRECTORY"
+  (let* ((dir directory)
+         result)
+    (while (not (string= dir (directory-file-name dir)))
+      (let* ((file-path (expand-file-name file-name dir)))
+        (when (file-exists-p file-path)
+          (setq result file-path))
+        (setq dir (file-name-directory (directory-file-name dir)))))
+    result))
 
 (setq private-c-cpp-project-search-text nil)
 
 (defun set-last-cpp-project-search-text (&rest args)
-    (unless private-c-cpp-project-search-text
-	(setq private-c-cpp-project-search-text (trim-prefix  (minibuffer-contents-no-properties) ?#))))
+  (unless private-c-cpp-project-search-text
+    (setq private-c-cpp-project-search-text (trim-prefix  (minibuffer-contents-no-properties) ?#))))
 
 (defun cpp-project-search-impl (dir)
-    (let* ((project-dir (or (locate-dominating-file dir "CMakeLists.txt")
-			    (user-error "cannot find CMakeLists.txt"))))
-	(progn 
-	    (add-hook 'minibuffer-exit-hook 'set-last-cpp-project-search-text)
-	    (advice-add 'vertico-exit :before 'set-last-cpp-project-search-text)
-	    (advice-add 'vertico-super-tab :before 'set-last-cpp-project-search-text)
-	    (let* ((initial-text private-c-cpp-project-search-text))
-		(setq private-c-cpp-project-search-text nil)
-		(unwind-protect
-		    (consult-ripgrep project-dir (or initial-text ""))
-		    (progn
-			(remove-hook 'minibuffer-exit-hook 'set-last-cpp-project-search-text)
-			(advice-remove 'vertico-exit 'set-last-cpp-project-search-text)
-			(advice-remove 'vertico-super-tab 'set-last-cpp-project-search-text)))))))
+  (let* ((project-dir (or (locate-dominating-file dir "CMakeLists.txt")
+			  (user-error "cannot find CMakeLists.txt"))))
+    (progn 
+      (add-hook 'minibuffer-exit-hook 'set-last-cpp-project-search-text)
+      (advice-add 'vertico-exit :before 'set-last-cpp-project-search-text)
+      (advice-add 'vertico-super-tab :before 'set-last-cpp-project-search-text)
+      (let* ((initial-text private-c-cpp-project-search-text))
+	(setq private-c-cpp-project-search-text nil)
+	(unwind-protect
+	    (consult-ripgrep project-dir (or initial-text ""))
+	  (progn
+	    (remove-hook 'minibuffer-exit-hook 'set-last-cpp-project-search-text)
+	    (advice-remove 'vertico-exit 'set-last-cpp-project-search-text)
+	    (advice-remove 'vertico-super-tab 'set-last-cpp-project-search-text)))))))
 
 (defun cpp-project-search ()
-    (interactive)
-    "find CMakeLists.txt and search the corresponding `src` folder"
-    (cpp-project-search-impl default-directory))
+  (interactive)
+  "find CMakeLists.txt and search the corresponding `src` folder"
+  (cpp-project-search-impl default-directory))
 
 
 
 (defun compile-cmake-project ()
-    (interactive)
-    (let* (
-	      (cmake-dir (locate-last-dominating-file default-directory "CMakeLists.txt"))
-	      (cmake-build-dir (expand-file-name "build" cmake-dir))
-	      (command (format "cmake -G Ninja -H\"%s\" -B \"%s\" && cmake --build \"%s\""  cmake-dir cmake-build-dir cmake-build-dir)))
-	(setenv "CMAKE_EXPORT_COMPILE_COMMANDS" "1")
-	(compile command))) 
+  (interactive)
+  (let* ((cmake-dir (locate-last-dominating-file default-directory "CMakeLists.txt"))
+         (cmake-build-dir (expand-file-name "build" cmake-dir))
+         (command (format "cmake -G Ninja -H\"%s\" -B \"%s\" && cmake --build \"%s\""  cmake-dir cmake-build-dir cmake-build-dir)))
+    (setenv "CMAKE_EXPORT_COMPILE_COMMANDS" "1")
+    (compile command))) 
 
 (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
 
@@ -81,44 +79,40 @@
 
 
 (defun re-compile-cmake-project ()
-    (interactive)
-    (clean-cmake-project)
-    (compile-cmake-project))
+  (interactive)
+  (clean-cmake-project)
+  (compile-cmake-project))
 
 (defun clean-cmake-project ()
-    (interactive)
-    (let*  (
-	       (cmake-dir (locate-last-dominating-file default-directory "CMakeLists.txt"))
-	       (cmake-build-dir (expand-file-name "build" cmake-dir)))
-	(delete-directory cmake-build-dir t)))
+  (interactive)
+  (let*  ((cmake-dir (locate-last-dominating-file default-directory "CMakeLists.txt"))
+	  (cmake-build-dir (expand-file-name "build" cmake-dir)))
+    (delete-directory cmake-build-dir t)))
 
 (setq cpp-project-actions
-    '(
-	("build" . compile-cmake-project)
+      '(("build" . compile-cmake-project)
 	("re-build" . re-compile-cmake-project)
 	("clean" . clean-cmake-project)))
 
 (defun cmake-project-action-menu ()
-    (interactive)
-    (let* (
-	      (action-name (completing-read "Action:" cpp-project-actions))
-	      (action (cdr (assoc action-name cpp-project-actions))))
-	(prin1 
-	    (funcall action))))
+  (interactive)
+  (let* ((action-name (completing-read "Action:" cpp-project-actions))
+         (action (cdr (assoc action-name cpp-project-actions))))
+    (funcall action)))
 
 
 
 (after-load 'evil
-    (evil-define-key 'normal c++-mode-map (kbd "SPC ,r") 'cpp-project-search)
-    (after-load 'smartparens 
-	(evil-define-key 'visual c++-mode-map (kbd "(") 'sp-wrap-round)
-	(evil-define-key 'visual c++-mode-map (kbd ")") 'sp-wrap-square)
-	(evil-define-key 'visual c++-mode-map (kbd "{") 'sp-wrap-curly)))
+  (evil-define-key 'normal c++-mode-map (kbd "SPC ,r") 'cpp-project-search)
+  (after-load 'smartparens 
+    (evil-define-key 'visual c++-mode-map (kbd "(") 'sp-wrap-round)
+    (evil-define-key 'visual c++-mode-map (kbd ")") 'sp-wrap-square)
+    (evil-define-key 'visual c++-mode-map (kbd "{") 'sp-wrap-curly)))
 
 (define-key c++-mode-map (kbd "C-c C-b") 'compile-cmake-project)
 (after-load 'cmake-mode 
-    (define-key cmake-mode-map (kbd "C-c C-b") 'compile-cmake-project)
-    (define-key cmake-mode-map (kbd "C-c b") 'cmake-project-action-menu))
+  (define-key cmake-mode-map (kbd "C-c C-b") 'compile-cmake-project)
+  (define-key cmake-mode-map (kbd "C-c b") 'cmake-project-action-menu))
 
 (define-key c-mode-map (kbd "C-c C-b") 'compile-cmake-project)
 
--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -7,93 +7,90 @@
 
 
 (after-load 'smartparens (sp-local-pair (list 'elisp-mode 'clojure-mode) "'" nil 
-			     :actions nil))
+				        :actions nil))
 
-
+(setq-default indent-tabs-mode nil)
+(put 'if 'lisp-indent-function 'defun)
+(setq lisp-indent-offset nil)
 (setq evil-elisp-mark nil)
 
 (add-hook 'evil-visual-state-exit-hook (lambda () (setq evil-elisp-mark nil)))
 
 (defun mark-outer-sexp ()
-    (interactive)
-    (progn 
-	(call-interactively 'backward-up-list)
-	(call-interactively 'evil-exit-visual-state)
-	(call-interactively 'evil-jump-item)
-	(call-interactively 'evil-visual-char)
-	(call-interactively 'evil-jump-item)))
+  (interactive)
+  (progn 
+    (call-interactively 'backward-up-list)
+    (call-interactively 'evil-exit-visual-state)
+    (call-interactively 'evil-jump-item)
+    (call-interactively 'evil-visual-char)
+    (call-interactively 'evil-jump-item)))
 
 
 (defun my-expand-region ()
-    (interactive)
-    (when (= (-  (region-end) (region-beginning)) 1 )
-	(call-interactively 'evil-exit-visual-state))
-    (call-interactively 'er/expand-region))
+  (interactive)
+  (when (= (-  (region-end) (region-beginning)) 1 )
+    (call-interactively 'evil-exit-visual-state))
+  (call-interactively 'er/expand-region))
 
 (defun my-forward-sexp ()
-    (interactive)
-    (if (= (point) (region-beginning))
-	(let* ((origin-point (point)))
-	    (goto-char (region-end))
-	    (backward-sexp -1)
-	    (let ((end-point (point)))
-		(set-mark origin-point)
-		(goto-char end-point)))
-	(backward-sexp -1)))
+  (interactive)
+  (if (= (point) (region-beginning))
+    (let* ((origin-point (point)))
+      (goto-char (region-end))
+      (backward-sexp -1)
+      (let ((end-point (point)))
+	(set-mark origin-point)
+	(goto-char end-point)))
+    (backward-sexp -1)))
 
 
-
-
-(setq lisp-indent-offset 4)
 
 (defun check-selected-text ()
-    "Obtains the selected text region in the editor as a string and checks if there is any space or line-break in it."
-    (interactive)
-    (if (string-match-p
-	    "[[:space:]\n]"
-	    (buffer-substring-no-properties
-		(region-beginning)
-		(region-end)))
-	t
-	nil))
+  "Obtains the selected text region in the editor as a string and checks if there is any space or line-break in it."
+  (interactive)
+  (if (string-match-p
+       "[[:space:]\n]"
+       (buffer-substring-no-properties
+	(region-beginning)
+	(region-end)))
+    t
+    nil))
 
 
 (defun enter-after-sexp ()
-    (interactive)
-    (let (
-	     (region-end-evil (region-end)))
-
-	(call-interactively 'evil-insert)
-	(goto-char (region-end))
-	(call-interactively 'paredit-RET)))
+  (interactive)
+  (let* ((region-end-evil (region-end)))
+    (call-interactively 'evil-insert)
+    (goto-char (region-end))
+    (call-interactively 'paredit-RET)))
 
 
 (defun lisp-expand-til-multiple-sexp ()
-    (interactive)
-    (while (not (check-selected-text))
-	(call-interactively 'my-expand-region)))
+  (interactive)
+  (while (not (check-selected-text))
+    (call-interactively 'my-expand-region)))
 
 (defun emacs-lisp-sexp-key ()
-    (evil-define-key 'visual clojure-mode-map (kbd "v") 'my-expand-region)
-    (evil-define-key 'visual clojure-mode-map (kbd "V") 'lisp-expand-til-multiple-sexp)
-    (evil-define-key 'visual clojure-mode-map (kbd "C-<return>") 'enter-after-sexp)
-    (evil-define-key 'visual clojure-mode-map (kbd "C-l") 'my-forward-sexp)
-    (evil-define-key 'visual clojure-mode-map (kbd "C-h") 'backward-sexp)
-    (evil-define-key 'insert clojure-mode-map (kbd "C-9") 'paredit-wrap-round-from-behind)
-    (evil-define-key 'visual emacs-lisp-mode-map (kbd "v") 'my-expand-region)
-    (evil-define-key 'visual emacs-lisp-mode-map (kbd "V") 'lisp-expand-til-multiple-sexp)
-    (evil-define-key 'visual emacs-lisp-mode-map (kbd "C-<return>") 'enter-after-sexp)
-    (evil-define-key 'visual emacs-lisp-mode-map (kbd "C-l") 'my-forward-sexp)
-    (evil-define-key 'visual emacs-lisp-mode-map (kbd "C-h") 'backward-sexp)
-    (evil-define-key 'insert emacs-lisp-mode-map (kbd "C-9") 'paredit-wrap-round-from-behind))
+  (evil-define-key 'visual clojure-mode-map (kbd "v") 'my-expand-region)
+  (evil-define-key 'visual clojure-mode-map (kbd "V") 'lisp-expand-til-multiple-sexp)
+  (evil-define-key 'visual clojure-mode-map (kbd "C-<return>") 'enter-after-sexp)
+  (evil-define-key 'visual clojure-mode-map (kbd "C-l") 'my-forward-sexp)
+  (evil-define-key 'visual clojure-mode-map (kbd "C-h") 'backward-sexp)
+  (evil-define-key 'insert clojure-mode-map (kbd "C-9") 'paredit-wrap-round-from-behind)
+  (evil-define-key 'visual emacs-lisp-mode-map (kbd "v") 'my-expand-region)
+  (evil-define-key 'visual emacs-lisp-mode-map (kbd "V") 'lisp-expand-til-multiple-sexp)
+  (evil-define-key 'visual emacs-lisp-mode-map (kbd "C-<return>") 'enter-after-sexp)
+  (evil-define-key 'visual emacs-lisp-mode-map (kbd "C-l") 'my-forward-sexp)
+  (evil-define-key 'visual emacs-lisp-mode-map (kbd "C-h") 'backward-sexp)
+  (evil-define-key 'insert emacs-lisp-mode-map (kbd "C-9") 'paredit-wrap-round-from-behind))
 
 (add-hook 'emacs-startup-hook 'emacs-lisp-sexp-key)
 
 (defun paredit-wrap-round-from-behind ()
-    (interactive)
-    (forward-sexp -1)
-    (paredit-wrap-round)
-    (forward-sexp 1)
-    (insert " "))
+  (interactive)
+  (forward-sexp -1)
+  (paredit-wrap-round)
+  (forward-sexp 1)
+  (insert " "))
 
 (provide 'init-elisp)
