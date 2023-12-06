f58bac81f9a9cb5d5a159d2a60cbef043ab2adf5
Donglin_Yang <yangdl@global.primarius-tech.com>
2023-12-06 10:48:26 +0800

    add quotes to cmake commands

--- lisp/init-c-cpp.el
+++ lisp/init-c-cpp.el
@@ -58,7 +58,7 @@
     (let* (
 	      (cmake-dir (locate-dominating-file default-directory "CMakeLists.txt"))
 	      (cmake-build-dir (expand-file-name "build" cmake-dir))
-	      (command (format "cmake -G Ninja -H%s -B %s && cmake --build %s" cmake-dir cmake-build-dir cmake-build-dir)))
+	      (command (format "cmake -G Ninja -H\"%s\" -B \"%s\" && cmake --build \"%s\""  cmake-dir cmake-build-dir cmake-build-dir)))
 	(setenv "CMAKE_EXPORT_COMPILE_COMMANDS" "1")
 	(compile command))) 
 
