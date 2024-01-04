542db3bdd055ef485867e8d7a48f03203600be44
Donglin_Yang <yangdl@global.primarius-tech.com>
2024-01-04 14:57:21 +0800

    update straight

--- lisp/preload.el
+++ lisp/preload.el
@@ -5,20 +5,29 @@
 
 ;; bootstrap straight.el
 (defvar bootstrap-version)
-(let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el"
-			  user-emacs-directory)) 
-	 (bootstrap-version 5)) 
-    (unless (file-exists-p bootstrap-file) 
-	(with-current-buffer (url-retrieve-synchronously
-				 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el" 'silent 'inhibit-cookies)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-	    (goto-char (point-max))
-	    (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
+
+
+
 
 ;; use package integration
 (straight-use-package 'use-package)
 (use-package el-patch :straight t)
+
 
 (setq read-process-output-max (* 16 1024 1024))
 
