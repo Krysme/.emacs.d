c4989676fb5f929ea263ef26517a56919f168a8b
Donglin_Yang <yangdl@global.primarius-tech.com>
2024-01-30 17:03:00 +0800

    fix company completion

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -8,20 +8,10 @@
 
 
 
-(defun lisp-select-only-one-or-select-current()
-  "How tab should behave when completing code"
-  (interactive)
-  (unless  (> company-candidates-length 1)
-    (call-interactively 'company-select-next)
-    (call-interactively 'company-complete-selection))
-  (if company-selection
-      (call-interactively 'company-complete-selection)
-    (call-interactively 'company-select-next)))
-
 
 (after-load 'company
-  (define-key company-active-map [tab] 'lisp-select-only-one-or-select-current)
-  (define-key company-active-map (kbd "TAB") 'lisp-select-only-one-or-select-current)
+  (define-key company-active-map [tab] 'company-complete-selection)
+  (define-key company-active-map (kbd "TAB") 'company-complete-selection)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "M-p") nil)
