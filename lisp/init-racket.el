baddb2aa44d44ef3d35ca86f48485bb65b4ce8e6
Krys <blackshop.s.bossss@gmail.com>
2025-11-27 14:22:07 +0800

    fix racket eval

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -2,11 +2,22 @@
 
 (straight-use-package 'racket-mode)
 
-(add-hook 'racket-mode 'racket-xp-mode)
 (add-hook 'racket-mode-hook (lambda () (define-key racket-mode-map (kbd "C-c C-r") 'racket-run)))
 
 (after-load 'racket-mode
         (define-key racket-mode-map (kbd "C-x C-e") 'racket-eval-last-sexp))
+
+(defun box-cursor-active-p ()
+       (cond
+        ((bound-and-true-p evil-local-mode) (eq evil-state 'normal))))
+
+(defun racket-eval-last-sexp-fixed ()
+       (interactive)
+       (save-excursion
+               (if (and (box-cursor-active-p) (memq (char-after) '(?\) ?\] ?\})))
+                       (forward-char))
+               (call-interactively 'racket-eval-last-sexp)))
+
 (add-hook 'racket-mode-hook 'paredit-mode)
 
 (add-hook 'racket-mode-hook 'company-mode)
