795e366fc3c6bee9298ec309113389370849885e
Krys <blackshop.s.bossss@gmail.com>
2026-01-07 21:03:36 +0800

    fix golang binding

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -14,15 +14,14 @@
 (add-hook 'go-mode-hook 'show-paren-mode)
 
 (add-hook 'go-mode-hook 
-	  (lambda () 
-	          (define-key go-mode-map (kbd "C-c C-b") 'compile-go-lsp) 
-	          (define-key go-mode-map (kbd "C-c C-r") 'run-go-lsp) 
-	          (define-key evil-visual-state-map (kbd "(") 'sp-wrap-round) 
-	          (define-key evil-visual-state-map (kbd ")") 'sp-wrap-square) 
-	          (define-key evil-visual-state-map (kbd "{") 'sp-wrap-curly) 
-	          (define-key evil-visual-state-map (kbd "<") 
-	                      (lambda () 
-		                      (interactive) 
-		                      (sp-wrap-with-pair "<")))))
+          (lambda () 
+                  (define-key go-mode-map (kbd "C-c C-b") 'compile-go-lsp) 
+                  (define-key go-mode-map (kbd "C-c C-r") 'run-go-lsp) 
+
+	          (evil-define-key 'visual go-mode-map (kbd "(") #'sp-wrap-round)
+                  (evil-define-key 'visual go-mode-map (kbd ")") #'sp-wrap-square)
+                  (evil-define-key 'visual go-mode-map (kbd "{") #'sp-wrap-curly)
+                  (evil-define-key 'visual go-mode-map (kbd "<")
+                          (lambda () (interactive) (sp-wrap-with-pair "<")))))
 
 (provide 'init-go)
