bfc07241909b97685c8044052fe3a08a7a1fdac2
Krys <blackshop.s.bossss@gmail.com>
2026-01-07 21:12:39 +0800

    remove comments

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,10 +1,9 @@
 
 (straight-use-package 'livedown)
-
 (custom-set-variables
- '(livedown-autostart nil) ; automatically open preview when opening markdown files
- '(livedown-open t)        ; automatically open the browser window
- '(livedown-port 1337)     ; port for livedown server
- '(livedown-browser nil))  ; browser to use
+ '(livedown-autostart nil) 
+ '(livedown-open t)        
+ '(livedown-port 1337)    
+ '(livedown-browser nil))
 
 (provide 'init-markdown)
