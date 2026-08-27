6d296f877900ea13a62bcafe0755528dd678da9c
Krys <blackshop.s.bossss@gmail.com>
2026-08-27 17:21:18 +0800

    remove cider in clojure-mode-hook

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -3,7 +3,6 @@
 (straight-use-package 'clojure-mode)
 
 
-(add-hook 'clojure-mode-hook 'cider)
 (after-load 'cider 
   (evil-define-key 'insert cider-repl-mode-map (kbd "C-l") 'cider-repl-clear-buffer)
   (evil-define-key 'insert cider-repl-mode-map (kbd "C-p") 'cider-repl-previous-input)
